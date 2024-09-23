95baa144340a2a60b4c647afe0cdb0b93ef570f2
Deo-Ekodi <deoekodi@gmail.com>
2024-09-23 06:47:17 -0800

    remove headefrfrom navbar

--- atc/app/components/Header.js
+++ atc/app/components/Header.js
@@ -4,7 +4,6 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <header>
-      <h1>ATC and Pilot Simulation</h1>
       <nav>
         <Link href="/">Home</Link>
         <Link href="/pilot">Pilot</Link>
